4aaad018372c2d7d49723b4e09ec2322778c7b3d
NGUYEN Minh <minh.nguyen@ddth.corp>
2023-08-02 14:45:07 +0200

    first commit

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Mouse-image-gallery',
+  description: 'Stunning mouse image gallery',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
